039ddf3a31e492080e0b8427300af03aa1553d4c
Lhakpa_S653 <88940543+Lhakpa-Sonam-Sherpa@users.noreply.github.com>
2025-02-09 00:16:56 +0545

    Update infixToPostfix.py
    updated a function that separates the operand and operator form the math expression.

--- infixToPostfix.py
+++ infixToPostfix.py
@@ -1,18 +1,28 @@
-number = ""
-operator = ""
-operator_array = []
-operand_array = []
-expression = input("Enter the math expression:\n\t")
-# expression = "123+12"
-for character in expression:
-    print(f"{character}")
-    if character.isdigit():
-        number = number + character
-    elif not character.isdigit():
-        operand_array.append(number)
-        operator_array.append(character)
-        number = ""
-    
-operand_array.append(number)
 
-print(f"operand: {operand_array}\noperator: {operator_array}")
+#operator and operand seperator
+def expression_separator(expression):
+    number = ""
+    operator = ""
+    operator_array = []
+    operand_array = []
+    expression_arr = []
+    for character in expression:
+        print(f"{character}")
+        if character.isdigit():
+            number = number + character
+        elif not character.isdigit():
+            operand_array.append(number)
+            operator_array.append(character)
+            number = ""
+        
+    operand_array.append(number)
+    expression_arr.append(operand_array)
+    expression_arr.append(operator_array)
+    return operand_array, operator_array
+
+
+
+expr = input("Enter the math expression:\n\t")
+# expr = "123+12"
+ops = expression_separator(expr)
+print(f"operand: {ops[0]}\noperator: {ops[1]}")
